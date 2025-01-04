41665a77c517a2b8dcbc8811132c54588866dca0
rom38 <rom38@github.com>
2025-01-04 19:04:18 +0800

    add map formar, conform

---
+++ configs/conform.lua
@@ -0,0 +1,30 @@
+local options = {
+  timeout_ms = 60000,
+  formatters_by_ft = {
+    lua = { "stylua" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    svelte = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    json = { "prettier" },
+    yaml = { "prettier" },
+    markdown = { "prettier" },
+    graphql = { "prettier" },
+    liquid = { "prettier" },
+    python = { "isort", "black" },
+    -- css = { "prettier" },
+    -- html = { "prettier" },
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 5000,
+    lsp_fallback = true,
+    async = false,
+  },
+}
+
+return options
--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,20 +1,20 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+-- local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
-local nvlsp = require "nvchad.configs.lspconfig"
+-- local servers = { "html", "cssls" }
+-- local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
+-- for _, lsp in ipairs(servers) do
+-- lspconfig[lsp].setup {
+-- on_attach = nvlsp.on_attach,
+-- on_init = nvlsp.on_init,
+-- capabilities = nvlsp.capabilities,
+-- }
+-- end
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
@@ -22,3 +22,40 @@ end
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
+--
+local configs = require "nvchad.configs.lspconfig"
+
+configs.defaults()
+
+local servers = {
+  html = {},
+  cssls = {},
+  ts_ls = {},
+  eslint = {},
+  -- tailwindcss = {},
+  emmet_ls = {
+    filetypes = { "css", "html", "javascript", "javascriptreact", "less", "sass", "scss", "typescriptreact" },
+  },
+  jsonls = {},
+  awk_ls = {},
+  bashls = {},
+
+  pyright = {
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          typeCheckingMode = "basic",
+        },
+      },
+    },
+  },
+}
+
+for name, opts in pairs(servers) do
+  opts.on_init = configs.on_init
+  opts.on_attach = configs.on_attach
+  opts.capabilities = configs.capabilities
+
+  require("lspconfig")[name].setup(opts)
+end
